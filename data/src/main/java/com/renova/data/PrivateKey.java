81b241f03f2112e2e1d0593215cb2bace4221480
Renova <dssxjp@gmail.com>
2016-03-09 14:55:42 +0800

    PrivateKey修改，添加MD5包名到weibo

---
+++ data/src/main/java/com/renova/data/PrivateKey.java
@@ -0,0 +1,9 @@
+package com.renova.data;
+
+/**
+ * Created by Renova on 2016/3/9.
+ */
+public class PrivateKey {
+    public static final String APP_KEY = "776879885";
+    public static final String APP_SECRET = "551ee2bad5017eafa98a9a3779985a0f";
+}
---
+++ data/src/main/java/com/renova/data/net/RestApi.java
@@ -0,0 +1,7 @@
+package com.renova.data.net;
+
+/**
+ * Created by Renova on 2016/3/9.
+ */
+public interface RestApi {
+}
